94c8f906fb792c70e1e0ac3fdf70e896631e21e0
Castelli87 <davidecastelli87@hotmail.com>
2023-05-09 15:28:37 +0100

    refactor the handle error and remove all the logs

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -3,7 +3,7 @@ const { selectTopics } = require("../models/topics.model");
 exports.getTopics = (req, res, next) => {
   selectTopics()
     .then((result) => {
-      console.log(result.rows);
+
       res.status(200).send({ topics: result.rows });
     })
 };
