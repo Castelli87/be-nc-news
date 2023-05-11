aa5175410e6a3bba4847d62758573b2e2f0b9319
Castelli87 <davidecastelli87@hotmail.com>
2023-05-11 11:17:44 +0100

    Add and change all the requests, now sending to the PR

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
+
 
 const { getTopics, getApi} = require("./controllers/topics.controller");
 
--- playground.sql
+++ playground.sql
@@ -1,12 +1,19 @@
 \c nc_news_test;
 
-SELECT * FROM articles ;
---SELECT articles.author,articles.title,articles.article_id,articles.topic,articles.created_at,articles.votes,articles.article_img_urlFROM articles,comments.article_id FROM articles JOIN articles.article_id=comments.article_id;
+SELECT 
+    articles.author, 
+    articles.title, 
+    articles.article_id, 
+    articles.topic, 
+    articles.created_at, 
+    articles.votes, 
+    articles.article_img_url,
 
-SELECT articles.author,articles.title,articles.article_id,articles.topic,articles.created_at,articles.votes,articles.article_img_url,
-COUNT(comments.comment_id) as comment_count
-FROM articles
-JOIN comments
-ON comments.article_id=articles.article_id
-GROUP BY articles.article_id
-ORDER BY created_at DESC;
+    COUNT(*)::INT AS comment_count
+    FROM articles
+
+    JOIN comments ON comments.article_id = articles.article_id
+
+    GROUP BY articles.article_id
+
+    ORDER BY articles.created_at DESC;
