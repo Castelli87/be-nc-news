b245784088f924c4e2db9ecc7524869c38e62ec6
Castelli87 <davidecastelli87@hotmail.com>
2023-05-11 12:16:20 +0100

    Ticket 6 Done

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,6 +2,11 @@ const {selectCommentsByArticleId}=require('../models/comments.model')
 
 exports.getCommentsByArticleId=(req,res,next)=>{
     const article_id = req.params.article_id
-    console.log(article_id,'<<<<<<<<controller')
-    selectCommentsByArticleId()
+    
+    selectCommentsByArticleId(article_id).then((result)=>{
+     
+        res.status(200).send({commentsByArticleId:result})
+    }).catch((err)=>{
+        next(err)
+    })
 }
